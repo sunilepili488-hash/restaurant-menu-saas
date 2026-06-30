18bbce67f5ed76ede133adc745f5e06871447726
sunilepili488-hash <sunilepili488@gmail.com>
2026-06-30 15:54:00 +0530

    Delete src/lib/ResetPassword.jsx

--- src/lib/ResetPassword.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-// Password reset is managed inside the restaurant settings (BrandingSection).
-export default function ResetPassword() {
-  return <Navigate to="/login" replace />;
-}
